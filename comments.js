6360013ff7cc8aba85a9f7e72c5ed6a06c2094ed
Heidi Housten <14905544+housten@users.noreply.github.com>
2024-04-10 13:02:59 +0000

    add comments file

---
+++ comments.js
@@ -0,0 +1,16 @@
+// create web server
+var express = require('express');
+var app = express();
+
+// create server
+var server = require('http').createServer(app);
+
+// handle request
+app.get('/', function(req, res) {
+  res.send('Hello world');
+});
+
+// listen
+server.listen(3000, function() {
+  console.log('Server is listening on port 3000');
+});
